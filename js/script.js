2ed1adb48398b12328ae096d4c17ff785933aa55
YanFelipy22 <yan-felipy@hotmail.com>
2021-10-05 01:26:19 -0300

    Iniciando portfolio, Header estilizado, responsivo e funcional

---
+++ js/script.js
@@ -0,0 +1,27 @@
+ // Botôes:
+ const btnMobile = document.getElementById('btn-mobile');
+ const btnMob = document.getElementById('btn-mob');
+
+
+ // Funções:
+
+ function toggleMenu(event) {
+     if (event.type === 'touchstart') event.preventDefault();
+
+     const nav = document.getElementById('nav');
+     nav.classList.toggle('active');
+
+     const active = nav.classList.contains('active');
+     event.currentTarget.setAttribute('aria-expanded', active);
+
+     if (active) event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+     else event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+
+ }
+
+ // Eventos:
+
+ btnMobile.addEventListener('click', toggleMenu);
+ btnMobile.addEventListener('touchstart', toggleMenu);
+ btnMob.addEventListener('click', toggleMenu);
+ btnMob.addEventListener('touchstart', toggleMenu);
