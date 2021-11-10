95708bb5a4328f1d082f3476011917b482ffbb45
YanFelipy22 <yan-felipy@hotmail.com>
2021-11-10 15:46:30 -0300

    Menu tecnologias

--- js/script.js
+++ js/script.js
@@ -2,10 +2,12 @@
 
  const btnMobile = document.getElementById('btn-mobile');
  const btnMob = document.getElementById('btn-mob');
+ const btnDown1 = document.getElementById('btn-down1');
+ const btnDown2 = document.getElementById('btn-down2');
 
  // Funções:
 
- function toggleMenu(event) {
+ function toggleMenu (event) {
      if (event.type === 'touchstart') event.preventDefault();
 
      const nav = document.getElementById('nav');
@@ -18,17 +20,35 @@
      else event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
 
  }
+ function toggleSkillFront (event) {
+    if (event.type === 'touchstart') event.preventDefault();
 
-function progressBar() {
-    const span = document.getElementsByClassName('percent');
-    const bar = document.getElementsByClassName('tech-bar');
-    Array.from(span).map(function(){
-        
-        
-    })
-    console.log(span);
+    const frontskills = document.getElementById('tech-skillsfront');
+    frontskills.classList.toggle('active');
+   
+    const active = frontskills.classList.contains('active');
+    event.currentTarget.setAttribute('aria-expanded', active);
+
+      if (active) event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+    else event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+
 }
-progressBar();
+
+function toggleSkillBack (event) {
+    if (event.type === 'touchstart') event.preventDefault();
+
+    const backskills = document.getElementById('tech-skillsback');
+    backskills.classList.toggle('active');
+
+     const active = backskills.classList.contains('active');
+    event.currentTarget.setAttribute('aria-expanded', active);
+
+    if (active) event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+    else event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+
+}
+
+
 
 
 
@@ -38,3 +58,9 @@ progressBar();
  btnMobile.addEventListener('touchstart', toggleMenu);
  btnMob.addEventListener('click', toggleMenu);
  btnMob.addEventListener('touchstart', toggleMenu);
+
+ btnDown1.addEventListener('click', toggleSkillFront);
+ btnDown1.addEventListener('touchstart', toggleSkillFront);
+ 
+ btnDown2.addEventListener('click', toggleSkillBack);
+ btnDown2.addEventListener('touchstart', toggleSkillBack);
